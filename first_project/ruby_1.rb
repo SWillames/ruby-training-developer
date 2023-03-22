573bb872180a5f45fa3dcce4b026161d7203abe7
Sergio Willames Oliveira Costa <sergiowoc@gmail.com>
2023-03-22 06:57:58 -0300

    first challenge code (#1)
    Co-authored-by: Sergio Costa <tp.sergio.willames@totvspartners.com.br>

---
+++ first_project/ruby_1.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome:"
+name = gets.chomp
+puts "Digite seu sobrbenome:"
+surname = gets.chomp
+puts "Digite sua idade:"
+age = gets.chomp
+
+puts "Olá #{name} #{surname}, Bem Vindo, sua idade é #{age}"
